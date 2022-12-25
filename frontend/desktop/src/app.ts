904495f732de23aace412364d50e712f3af307de
MartinaeyNL <martinpeeters34@yahoo.nl>
2022-12-25 01:34:28 +0100

    Added shoelace as component library

--- frontend/desktop/src/app.ts
+++ frontend/desktop/src/app.ts
@@ -3,6 +3,9 @@ import {customElement} from 'lit/decorators.js'
 import {choose} from 'lit/directives/choose.js';
 import "./pages/page-dashboard";
 
+import shoelaceLightStyle from "@shoelace-style/shoelace/dist/themes/light.styles.js"
+import shoelaceDarkStyle from "@shoelace-style/shoelace/dist/themes/dark.styles.js";
+
 //language=css
 const styles = css`
     :host {
@@ -13,21 +16,23 @@ const styles = css`
 @customElement('toolboard-app')
 export class App extends LitElement {
 
-    static styles = [styles];
+    static styles = [shoelaceLightStyle, shoelaceDarkStyle, styles];
 
     render() {
         const page = "dashboard"
         return html`
-            <div style="display: flex; height: 100%;">
-                <div style="width: 20vw; min-width: 300px; max-width: 420px; background: #707070;">
-                    <span>Sidebar</span>
+            <app class="sl-theme-dark">
+                <div style="display: flex; height: 100%;">
+                    <div style="width: 20vw; min-width: 300px; max-width: 420px; background: #707070;">
+                        <span>Sidebar</span>
+                    </div>
+                    <div style="flex: 1;">
+                        ${choose(page, [
+                            ['dashboard', () => html`<page-dashboard></page-dashboard>`]
+                        ])}
+                    </div>
                 </div>
-                <div style="flex: 1;">
-                    ${choose(page, [
-                        ['dashboard', () => html`<page-dashboard></page-dashboard>`]
-                    ])}
-                </div>
-            </div>
+            </app>
         `
     }
 }
---
+++ frontend/desktop/types/pages/page-dashboard.d.ts
@@ -0,0 +1,5 @@
+import { LitElement } from "lit";
+export declare class PageDashboard extends LitElement {
+    static styles: never[];
+    protected render(): import("lit-html").TemplateResult<1>;
+}
