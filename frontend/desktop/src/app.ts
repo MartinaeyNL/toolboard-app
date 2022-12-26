6fad22deb28893f5bc7edb83e1f1a30bc0f1bdc6
MartinaeyNL <martinpeeters34@yahoo.nl>
2022-12-26 18:00:25 +0100

    Upgraded yarn to v3 and added yarn workspace foreach commands. Also fixed shoelace dependency, and added hot reloading for components in different folders.

--- frontend/desktop/src/app.ts
+++ frontend/desktop/src/app.ts
@@ -2,8 +2,10 @@ import {css, html, LitElement} from 'lit'
 import {customElement} from 'lit/decorators.js'
 import {choose} from 'lit/directives/choose.js';
 import "./pages/page-dashboard";
-import "@toolboard/tb-menu";
+import '@toolboard/tb-menu';
 
+// Import of all shoelace components
+import '@shoelace-style/shoelace';
 // @ts-ignore
 import shoelaceLightStyle from "@shoelace-style/shoelace/dist/themes/light.styles.js"
 // @ts-ignore
@@ -28,7 +30,6 @@ export class App extends LitElement {
             <app class="sl-theme-dark">
                 <div style="display: flex; height: 100%;">
                     <div style="width: 20vw; min-width: 300px; max-width: 420px; background: #707070;">
-                        <span>Sidebar</span>
                         <tb-menu></tb-menu>
                     </div>
                     <div style="flex: 1;">
